e5a61764de3a986b10e86efd9f3a27a6372f0a6c
TasmVanum <gaston.cath14@gmail.com>
2023-06-01 00:31:38 -0400

    vista de buses

---
+++ public/modovista.js
@@ -0,0 +1,22 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+const oscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
+
+const claro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+}
+
+const cambiarTema = () => {
+    document.querySelector("body").getAttribute("data-bs-theme") === "light" ?
+    oscuro() : claro ();
+}

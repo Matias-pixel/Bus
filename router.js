d40833485c04cf91879ad40b7d72e66a1f335af1
TasmVanum <gaston.cath14@gmail.com>
2023-06-01 11:48:51 -0400

    superuser

--- router.js
+++ router.js
@@ -26,6 +26,17 @@ router.get('/respaldo',(req, res)=>{
     res.render('respaldo');
 });
 
+router.get('/superadmin', (req, res) => {
+
+    conexion.query('SELECT usuario.id, usuario.nombre, usuario.correo, estadousuario.nombre AS estadouser, tipousuario.nombre AS tipouser FROM usuario INNER JOIN tipousuario ON tipousuario.id = usuario.tipo_usuario_id_fk INNER JOIN estadousuario ON estadousuario.id = usuario.estado_usuario_id_fk',(error , results) =>{
+        if(error){
+            throw error;
+        }else{
+            res.render('superadmin' ,{results:results})
+        }
+    })
+})
+
 
 const crud = require('./controllers/crud');
 

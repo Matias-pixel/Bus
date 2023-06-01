ff415595b4a01a39ab9f72d3d85222f79532906d
Matias-pixel <marchantmatias706@gmail.com>
2023-06-01 12:43:54 -0400

    aaaaaaaa

--- router.js
+++ router.js
@@ -28,11 +28,48 @@ router.get('/respaldo',(req, res)=>{
 
 router.get('/superadmin', (req, res) => {
 
-    conexion.query('SELECT usuario.id, usuario.nombre, usuario.correo, estadousuario.nombre AS estadouser, tipousuario.nombre AS tipouser FROM usuario INNER JOIN tipousuario ON tipousuario.id = usuario.tipo_usuario_id_fk INNER JOIN estadousuario ON estadousuario.id = usuario.estado_usuario_id_fk',(error , results) =>{
+    conexion.query('SELECT usuario.id, usuario.nombre, usuario.correo, usuario.pass, estadousuario.nombre as estadoNombre, tipousuario.nombre as tipoNombre from usuario INNER JOIN estadousuario ON estadousuario.id = usuario.estado_usuario_id_fk inner join tipousuario ON tipousuario.id = usuario.tipo_usuario_id_fk WHERE estado_usuario_id_fk = 1 AND tipo_usuario_id_fk != 1',(error , results) =>{
         if(error){
             throw error;
         }else{
             res.render('superadmin' ,{results:results})
+        }
+    })
+})
+
+//RUTA PARA 
+
+router.get('/deleteUser/:id', (req, res)=>{
+
+    const id = req.params.id;
+    conexion.query('UPDATE usuario SET estado_usuario_id_fk = 2 WHERE id = ? ', [id], (error)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/superadmin')
+        }
+    })
+})
+
+router.get('/usuariosDes', (req, res) => {
+
+    conexion.query('SELECT usuario.id, usuario.nombre, usuario.correo, usuario.pass, estadousuario.nombre as estadoNombre, tipousuario.nombre as tipoNombre from usuario INNER JOIN estadousuario ON estadousuario.id = usuario.estado_usuario_id_fk inner join tipousuario ON tipousuario.id = usuario.tipo_usuario_id_fk WHERE estado_usuario_id_fk = 2',(error , results) =>{
+        if(error){
+            throw error;
+        }else{
+            res.render('usuariosDes' ,{results:results})
+        }
+    })
+})
+
+router.get('/habilitarUser/:id', (req, res)=>{
+
+    const id = req.params.id;
+    conexion.query('UPDATE usuario SET estado_usuario_id_fk = 1 WHERE id = ? ', [id], (error)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/superadmin')
         }
     })
 })

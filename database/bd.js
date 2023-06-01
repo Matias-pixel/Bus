22d9014d6047f16f7dbc2d02e2943d428385ef02
Matias-pixel <marchantmatias706@gmail.com>
2023-06-01 01:40:23 -0400

    b

---
+++ database/bd.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bus'
+});
+
+
+conexion.connect((error)=>{
+    if(error){
+        console.log('Error al conectar con la base de datos');
+        return
+    }
+    console.log('Conectado con la base de datos bus');
+});
+
+module.exports = conexion;
---
+++ router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const conexion = require('./database/bd');
+const router = express.Router();
+
+router.get('/',  (req, res)=>{
+    res.render('login');
+})
+
+module.exports = router;
